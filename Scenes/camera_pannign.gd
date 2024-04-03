f22f6bd8e65f4b0115b1591c070c8e8230a08d38
Hedgein <hedgejess@gmail.com>
2024-04-02 20:29:04 -0500

    updates to snappier camera, changed townsperson

--- Scenes/camera_pannign.gd
+++ Scenes/camera_pannign.gd
@@ -8,8 +8,8 @@ func _ready():
 	var tile_size = tilemap.cell_quadrant_size
 	var world_size = map_rect.size * tile_size
 	
-	limit_right = world_size.x / 5
-	limit_bottom = world_size.y / 5
+	limit_right = world_size.x / 6
+	limit_bottom = world_size.y / 6
 	
 	print(limit_right)
 	print(limit_bottom)
@@ -19,10 +19,10 @@ func _process(delta):
 	position = player.position
 	var x = floor(position.x / 480) * 480
 	var y = floor(position.y / 240 ) * 240
-	if x > floor(limit_right * 0.5):
-		x = floor(limit_right *0.5)
-	if y > floor(limit_bottom * 0.5):
-		y = floor(limit_bottom *0.5)
+	if x > floor(limit_right * 0.7):
+		x = floor(limit_right *0.7)
+	if y > floor(limit_bottom * 0.7):
+		y = floor(limit_bottom *0.7)
 	position = Vector2(x,y)
 	var tween := create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(self, "position", Vector2(x,y), 0.14)
+	tween.tween_property(self, "position", Vector2(x,y), 0.10)
